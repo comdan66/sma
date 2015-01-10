04206f03b67c45f50725f42e84967c96a50df203
OA Wu <comdan66@gmail.com>
2015-01-10 10:48:57 +0800

    Temp

--- application/views/frame/site/frame.js
+++ application/views/frame/site/frame.js
@@ -8,4 +8,22 @@ $(function () {
       $('.content').css ({'height': $('#content').height () + 'px'});
   }).resize ();
   
+
+  var $rightSlide = $('#right_slide');
+  var overflow = $('body').css ('overflow');
+  $('.option').click (function () {
+    if ($rightSlide.hasClass ('close')) {
+      $rightSlide.removeClass ('close');
+      $('body').css ('overflow', 'hidden');
+    } else {
+      $rightSlide.addClass ('close');
+      $('body').css ('overflow', overflow);
+    }
+  });
+  $('#slide_cover').click (function () {
+    if (!$rightSlide.hasClass ('close')) {
+      $rightSlide.addClass ('close');
+      $('body').css ('overflow', overflow);
+    }
+  });
 });
--- application/views/frame/site/frame.php
+++ application/views/frame/site/frame.php
@@ -13,20 +13,47 @@
   </head>
   <body lang="zh-tw">
     <?php echo isset ($hidden) ? $hidden : '';?>
+
+    <div id='right_slide' class='close'>
+      <a href=''><div class='item'>ABOUT</div></a>
+      <a href=''><div class='item'>CASE STUDY</div></a>
+      <div class='sub show'>
+        <a href=''><div class='item'>dasds</div></a>
+        <a href=''><div class='item'>SHOWSHOWSHOWSHOWSHOWSHOWSHOWSHOW</div></a>
+        <a href=''><div class='item'>dasds</div></a>
+      </div>
+      <a href=''><div class='item'>dasds</div></a>
+      <a href=''><div class='item'>dasds</div></a>
+      <a href=''><div class='item'>dasds</div></a>
+      <a href=''><div class='item'>dasds</div></a>
+      <a href=''><div class='item'>dasds</div></a>
+    </div>
+    <div id='slide_cover'></div>
+
     <div id='container'>
       <div id='header' class='clearfix'>
         <div class='logo'>
           <img src='<?php echo base_url (array ('resource', 'site', 'img', 'wlogo.png'));?>'>
         </div>
         <div class='info'>
-          <img src='<?php echo base_url (array ('resource', 'site', 'img', 'title_bg.jpg'));?>' />
+          <img class='pc' src='<?php echo base_url (array ('resource', 'site', 'img', 'title_bg.jpg'));?>' />
+          <img class='mv' src='<?php echo base_url (array ('resource', 'site', 'img', 'mtitle_bg.jpg'));?>' />
           <div class='title'>CASE STUDY</div>
+          <div class='option'>
+            <img src='<?php echo base_url (array ('resource', 'site', 'img', 'menu_iocn.png'));?>'>
+          </div>
         </div>
       </div>
-      <div id='content' class='clearfix'>
+
+
+      <div id='content'>
         <div class='footer'>
-          <a href="https://www.facebook.com/chitorch922" class="icon-facebook">asd</a>
-          <div></div>
+          <div class='icons clearfix'>
+            <div class='icon'><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path d="M15 4.129h-2.856c-0.338 0-0.715 0.444-0.715 1.039v2.062h3.571v2.94h-3.571v8.83h-3.372v-8.829h-3.057v-2.941h3.057v-1.73c0-2.481 1.722-4.5 4.086-4.5h2.857v3.129z" fill="#444444"></path></svg></div>
+            <div class='line'></div>
+            <div class='icon'><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="0 0 32 32"><path d="M15.996 15.457l16.004-7.539v-3.918h-32v3.906zM16.004 19.879l-16.004-7.559v15.68h32v-15.656z" fill="#444444"></path></svg></div>
+          </div>
+          <div class='info'>© 2014 Scent Marketing Association Co.,Ltd.</div>
         </div>
         <div class='slider'>
           <a href=''><div class='item'>dasds</div></a>

ec8227e9fd484dafe24e7f97059687143675e467
OA Wu <comdan66@gmail.com>
2015-01-13 00:40:26 +0800

    Temp

---
+++ application/views/content/site/contacts/index/content.php
@@ -0,0 +1,14 @@
+
+<div id="contitle">
+  <p class="toptitle">嗅覺香氛行銷協會</p>
+  <p class="etoptitle">SMA Scent Marketing Association</p>
+</div>
+
+<div id="conbox">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.7747908363276!2d121.55114810000002!3d25.041715800000013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abc50a2471e1%3A0xfade3f39d519b50a!2zMTA25Y-w5YyX5biC5aSn5a6J5Y2A5b-g5a2d5p2x6Lev5Zub5q61MTkx6Jmf!5e0!3m2!1szh-TW!2stw!4v1421075504708" width="843" height="410" frameborder="0" style="border:0"></iframe>
+</div>
+
+<div id="conadd">
+  <p class="etoptitleadd">台北市大安區 忠孝東路四段 191 號 10樓之 1<br/>
+  10F.-1, No.191, Sec. 4, Jhongsiao E. Rd., Da-an District, Taipei City 10690, Taiwan (R.O.C.)</p>
+</div>

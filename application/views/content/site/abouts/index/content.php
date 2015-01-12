174b965e20c1d8009c2e27c475993ba6aa5b0162
OA Wu <comdan66@gmail.com>
2015-01-13 00:26:01 +0800

    Temp

---
+++ application/views/content/site/abouts/index/content.php
@@ -0,0 +1,50 @@
+
+<div id="atopbg">
+  <p class="atoptitle">&nbsp;&nbsp;&nbsp;ABOUT&nbsp;&nbsp;&nbsp;</p>
+</div>
+    
+    <div id="acontit">
+      <p class="chtitle">協會的宗旨</p>
+    </div>
+    
+<div class="chtitlea">本會為依法設立、非以營利為目的之社會團體，</br>
+以推動香氛應用於各種空間的普及化，提升生活品質及環境幸福感為宗旨。</div>
+
+<div id="acolorb">
+
+  <div class="acotob">
+    <p class="acotobo">協會的任務一</p>
+    <p class="acotobog">
+    定期舉辦香氛</br>
+    應用研討會議</br>
+    提升社會認知及專業應用</br>
+    </p>
+  </div>
+  
+   <div class="acotob">
+    <p class="acotobo">協會的任務二</p>
+    <p class="acotobog">
+    舉辦香氛專業人員之</br>
+    教育訓練及</br>
+    相關交流活動</br>
+    </p>
+  </div>
+  
+   <div class="acotob">
+    <p class="acotobo">協會的任務三</p>
+    <p class="acotobog">
+    提升台灣民眾對</br>
+  香氛使用的正確認知</br>
+    </p>
+  </div>
+  
+   <div class="acotob">
+    <p class="acotobo">協會的任務四</p>
+    <p class="acotobog">
+    與國際上香氛機構接軌</br>
+    提供台灣香氛產業</br>
+    最新資訊</br>
+    </p>
+  </div>
+  
+    </div>

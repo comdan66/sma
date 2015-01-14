79e14713277e3c1a1c123cc20f14bfe6a503443f
OA Wu <comdan66@gmail.com>
2015-01-14 22:24:58 +0800

    Temp

--- application/migrations/014_add_columns.php
+++ application/migrations/014_add_columns.php
@@ -5,7 +5,7 @@
  * @copyright   Copyright (c) 2015 OA Wu Design
  */
 
-class Migration_Add_columns extends CI_Migration {
+class Migration_Add_column2s extends CI_Migration {
   public function up () {
     $sql = "ALTER TABLE `new_blocks` ADD `youtube` varchar(255) COLLATE utf8_unicode_ci NOT NULL";
     $this->db->query ($sql);

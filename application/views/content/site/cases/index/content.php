8591c610fac67cdf72d134c069bcf5ebafd443d4
OA Wu <comdan66@gmail.com>
2015-03-26 19:43:18 +0800

    Fix bug.

--- application/views/content/site/cases/index/content.php
+++ application/views/content/site/cases/index/content.php
@@ -3,17 +3,19 @@
     </div>
     <div class='cases clearfix'>
 <?php if ($cases) {
-        foreach ($cases as $key => $case) { ?>
-          <a href='<?php echo base_url (array ('case', $case->id));?>' class='case_a' data-tag='<?php echo $case->tag ? $case->tag->name : '';?>'>
-            <div class='case'>
-              <img src="<?php echo $case->first_pic->file_name->url ('200x200');?>" />
-              <div class='name'><?php echo $case->title;?></div>
-              <div class='info clearfix'>
-                <div class='address'><?php echo $case->address;?></div>
-                <div class='level'><?php echo $case->level;?>坪</div>
+        foreach ($cases as $key => $case) {
+          if ($case->first_pic) { ?>
+            <a href='<?php echo base_url (array ('case', $case->id));?>' class='case_a' data-tag='<?php echo $case->tag ? $case->tag->name : '';?>'>
+              <div class='case'>
+                <img src="<?php echo $case->first_pic->file_name->url ('200x200');?>" />
+                <div class='name'><?php echo $case->title;?></div>
+                <div class='info clearfix'>
+                  <div class='address'><?php echo $case->address;?></div>
+                  <div class='level'><?php echo $case->level;?>坪</div>
+                </div>
               </div>
-            </div>
-          </a>
-  <?php }
+            </a>
+    <?php }
+        }
       } ?>
     </div>
